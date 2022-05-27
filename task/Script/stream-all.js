2005549c6d9fb78c186388e33f7aac37aaec7e92
crocodilian <57382505+aa75017730@users.noreply.github.com>
2022-05-27 22:13:00 +0800

    Update stream-all.js

--- task/Script/stream-all.js
+++ task/Script/stream-all.js
@@ -22,7 +22,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: '流媒体解锁检测',
       content: '',
-      icon: 'play.tv.fill',
+      icon: '4k.tv.fill',
       'icon-color': '#754FE6',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
