6f1acf25998bbe146de63789a889af82898372c0
crocodilian <57382505+aa75017730@users.noreply.github.com>
2022-05-27 09:41:46 +0800

    Update Timecard.js

--- task/Script/Timecard.js
+++ task/Script/Timecard.js
@@ -79,7 +79,7 @@ function datenotice() {
 }
 $done({
 title:"节假日倒计时",
-icon:"list.dash.header.rectangle",
+icon:"arrow.up.arrow.down",
 'icon-color': "#754FE6",
 content:tlist[nowlist][0]+"  :  "+today(tnumcount(nowlist))+"天\n"+tlist[Number(nowlist) + Number(1)][0] +"  :  "+ tnumcount(Number(nowlist) + Number(1))+ "天\n"+tlist[Number(nowlist) + Number(2)][0]+"  :  "+tnumcount(Number(nowlist) + Number(2))+"天"
 })
